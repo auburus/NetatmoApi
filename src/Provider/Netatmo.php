391574efa9db1b85ab557e202004bb9dc1317dd7
Jordi <jordi@weathercloud.net>
2015-11-30 14:51:22 +0100

    Comented code

--- src/Provider/Netatmo.php
+++ src/Provider/Netatmo.php
@@ -10,10 +10,16 @@ use Auburus\OAuth2\Client\Provider\NetatmoResourceOwner;
 
 class Netatmo extends AbstractProvider
 {
+    /**
+     * {@inheritdoc}
+     */
     public function getBaseAuthorizationUrl() {
         return 'https://api.netatmo.net/oauth2/authorize';
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getBaseAccessTokenUrl(array $params) {
         return 'https://api.netatmo.net/oauth2/token';
     }
@@ -22,19 +28,29 @@ class Netatmo extends AbstractProvider
      * This funcion only will work if the token is authorized
      * for the read_station scope. 
      * This are the urls depending on the scope.
+     * {@inheritdoc}
      */
     public function getResourceOwnerDetailsUrl(AccessToken $token) {
         return 'https://api.netatmo.net/api/getstationsdata?access_token=' . $token;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getDefaultScopes() {
         return ['read_station'];
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getScopeSeparator() {
         return ' ';
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function checkResponse(ResponseInterface $response, $data) {
         if ($response->getStatusCode() >= 400) {
             throw new IdentityProviderException(
@@ -45,6 +61,9 @@ class Netatmo extends AbstractProvider
         }
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function createResourceOwner(array $response, AccessToken $token)
     {
         return new NetatmoResourceOwner($response['body']['user']);
